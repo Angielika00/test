a2a8d8940891ec6010b058efd7cf8a57b9ada513
YOUR NAME <YOU@EXAMPLE.COM>
2017-11-10 07:39:30 -0600

    add archivo

---
+++ src/prueba/PruebaSlack.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author angie
+ */
+public class PruebaSlack {
+    
+    private String descripcion;
+    private String nombreCanal;
+    private int invitados;
+    private String mensajes;
+
+    public PruebaSlack() {
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNombreCanal() {
+        return nombreCanal;
+    }
+
+    public void setNombreCanal(String nombreCanal) {
+        this.nombreCanal = nombreCanal;
+    }
+
+    public int getInvitados() {
+        return invitados;
+    }
+
+    public void setInvitados(int invitados) {
+        this.invitados = invitados;
+    }
+
+    public String getMensajes() {
+        return mensajes;
+    }
+
+    public void setMensajes(String mensajes) {
+        this.mensajes = mensajes;
+    }
+    
+    
+    
+    
+    
+}
